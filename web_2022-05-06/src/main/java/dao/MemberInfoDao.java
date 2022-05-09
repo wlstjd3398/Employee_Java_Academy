5a78a64b9a111ec0014c1731a7ecf4a9cb3ce244
wlstjd3398 <wlstjd3398@naver.com>
2022-05-09 15:33:54 +0900

    validator, postman

--- web_2022-05-06/src/main/java/dao/MemberInfoDao.java
+++ web_2022-05-06/src/main/java/dao/MemberInfoDao.java
@@ -2,6 +2,7 @@ package dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import util.Database;
@@ -43,7 +44,45 @@ public class MemberInfoDao {
 			db.closeConnection(conn);
 			
 		}
-		
 	}
+	
+	
+//	public MemberInfo login(MemberInfo memberInfo) {
+//		
+//		Database db = new Database();
+//		
+//		Connection conn = db.getConnection();
+//		PreparedStatement pstmt = null;
+//		
+//		ResultSet rs = null;
+//		
+//		try {
+//			
+//			String sql = "SELECT * FROM member_info WHERE id = ? AND pw = ?";
+//			
+//			pstmt = conn.prepareStatement(sql);
+//			pstmt.setString(1, memberInfo.getId());
+//			pstmt.setString(2, memberInfo.getPw());
+//			
+//			
+//			rs = pstmt.executeQuery();
+//			
+//			
+//			//아디랑 비번을 찾았다면
+//			if(rs.next()) {
+//				// 조회한 회원 정보를 매개변수로 받은 객체에 담음
+//				String name = rs.getString("name");
+//				
+//				memberInfo.setName(name);
+//			}
+//		} catch (SQLException e) {
+//			e.printStackTrace();
+//		} finally {
+//			db.closeResultSet(rs);
+//			db.closePstmt(pstmt);
+//			db.closeConnection(conn);
+//		}
+//		return memberInfo;
+//	}
 	
 }
--- web_2022-05-06/src/main/java/service/MemberService.java
+++ web_2022-05-06/src/main/java/service/MemberService.java
@@ -18,6 +18,23 @@ public class MemberService {
 		}else {
 			return 400;
 		}
+	}
+
+	
+//	public int login(MemberInfo memberInfo) {
+//		
+//		MemberInfoDao dao = new MemberInfoDao();
+//		
+//		boolean result = dao.login(memberInfo);
+//		
+//		if(result) {
+//			return 200;
+//		}else {
+//			return 400;
+//		}
+//	}
+	
+	public int duplicate() {
 		
 		
 	}
