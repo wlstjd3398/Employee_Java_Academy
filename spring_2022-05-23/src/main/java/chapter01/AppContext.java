17d3c8ede93cb99d8b2e96c02139c5097e8dcf08
wlstjd3398 <wlstjd3398@naver.com>
2022-05-24 14:50:39 +0900

    스프링컨테이너, 스프링DI(일반적인 프로그래밍에서 의존)

--- spring_2022-05-23/src/main/java/chapter01/AppContext.java
+++ spring_2022-05-23/src/main/java/chapter01/AppContext.java
@@ -7,9 +7,23 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class AppContext {
 
-	//
+	// 해당 메서드가 생성한 객체를 스프링이 관리하는 빈 객체로 등록
 	@Bean
 	public Greeter greeter() {
+		Greeter g = new Greeter();
+		g.setFormat("%s, 드디어 시작!");
+		return g;
+	}
+	
+	@Bean
+	public Greeter greeter1() {
+		Greeter g = new Greeter();
+		g.setFormat("%s, 드디어 시작!");
+		return g;
+	}
+	
+	@Bean
+	public Greeter greeter2() {
 		Greeter g = new Greeter();
 		g.setFormat("%s, 드디어 시작!");
 		return g;
---
+++ spring_2022-05-23/src/main/java/chapter01/Ex02.java
@@ -0,0 +1,13 @@
+package chapter01;
+
+public class Ex02 {
+
+	public static void main(String[] args) {
+		
+		Singleton obj1 = Singleton.getInstance();
+		Singleton obj2 = Singleton.getInstance();
+		
+		System.out.println(obj1 == obj2);
+		
+	}
+}
