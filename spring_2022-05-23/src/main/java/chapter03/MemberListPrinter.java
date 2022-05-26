c926ddd0923c25aa76a079e4ac7317915ba803bd
wlstjd3398 <wlstjd3398@naver.com>
2022-05-26 14:09:22 +0900

    스프링 자동의존주입

---
+++ spring_2022-05-23/src/main/java/chapter03/MemberListPrinter.java
@@ -0,0 +1,41 @@
+package chapter03;
+
+import java.util.Collection;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+
+public class MemberListPrinter {
+	
+	private MemberDao memberDao;
+	private MemberPrinter memberPrinter;
+	
+//	public MemberListPrinter(MemberDao memberDao, MemberPrinter memberPrinter) {
+//		this.memberDao = memberDao;
+//		this.memberPrinter = memberPrinter;
+//	}
+	@Autowired
+	public void setMemberDao(MemberDao memberDao) {
+		this.memberDao = memberDao;
+	}
+	@Autowired
+	@Qualifier("printer1")
+	public void setMemberPrinter(MemberPrinter memberPrinter) {
+		this.memberPrinter = memberPrinter;
+	}
+	
+	public void printAll() {
+		Collection<Member> members = memberDao.selectAll();
+		
+		for(Member member : members) {
+			memberPrinter.print(member);
+		}
+		
+		System.out.println();
+		
+		
+	}
+
+	
+	
+}
---
+++ spring_2022-05-23/src/main/java/chapter03/MemberRegisterService.java
@@ -0,0 +1,47 @@
+package chapter03;
+
+import java.time.LocalDateTime;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+
+import exception.DuplicateMemberException;
+
+public class MemberRegisterService {
+
+	// 자동 의존 주입 대상 객체에  @Qualifier 애노테이션을 사용하면
+	// 이러한 한정자를 갖는 Bean 객체를 자동 의존 주입해라가 됨
+	@Autowired
+	@Qualifier("memberDao")
+	private MemberDao memberDao;
+	
+//	public MemberRegisterService(MemberDao memberDao) {
+//		this.memberDao = memberDao;
+//	}
+	
+	public long regist(RegisterRequest req) throws DuplicateMemberException{
+		// 이메일로 회원 정보 조회
+		Member member = memberDao.selectByEmail(req.getEmail());
+		
+		if(member != null) {
+			// 같은 이메일을 가진 회원이 존재한다면 예외 발생
+			throw new DuplicateMemberException("이미 사용중인 이메일입니다.");
+		}
+		
+		// 같은 이메일을 가진 회원이 존재하지 않으면 DB에 저장
+		Member newMember = new Member(req.getEmail(), req.getPassword(), req.getName(), LocalDateTime.now());
+		
+		memberDao.insert(newMember);
+		
+		return newMember.getId();
+	}
+
+	public MemberDao getMemberDao() {
+		return memberDao;
+	}
+
+	public void setMemberDao(MemberDao memberDao) {
+		this.memberDao = memberDao;
+	}
+	
+}
