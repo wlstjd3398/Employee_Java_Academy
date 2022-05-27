273676a5b17e0b7cd43d1788e869a774d00eac6a
wlstjd3398 <wlstjd3398@naver.com>
2022-05-27 14:20:06 +0900

    스프링 컴포넌트 스캔, 스프링 빈 라이프사이클과 범위

---
+++ spring_2022-05-23/src/main/java/chapter04/MemberInfoPrinter.java
@@ -0,0 +1,34 @@
+package chapter04;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+
+import exception.MemberNotFoundException;
+
+public class MemberInfoPrinter {
+	private MemberDao memberDao;
+	private MemberPrinter memberPrinter;
+	
+	@Autowired
+	public void setMemberDao(MemberDao memberDao) {
+		this.memberDao = memberDao;
+	}
+	@Autowired
+	@Qualifier("printer2")
+	public void setMemberPrinter(MemberPrinter memberPrinter) {
+		this.memberPrinter = memberPrinter;
+	}
+	
+	public void printMemberInfo(String email) throws MemberNotFoundException{
+		Member member = memberDao.selectByEmail(email);
+		if(member == null) {
+			throw new MemberNotFoundException();
+		}
+		
+		memberPrinter.print(member);
+		System.out.println();
+	}
+	
+	
+	
+}
