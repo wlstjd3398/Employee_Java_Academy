b0c0134a24646f8b4b2d985b6d10eb1dfc46fe0f
wlstjd3398 <wlstjd3398@naver.com>
2022-06-15 14:16:32 +0900

    스프링 MVC4 : 스프링이 제공하는 form 태그 hidden,select,options, 메세지, 커맨드 객체 검증,

---
+++ spring_2022-05-23/src/main/java/chapter11/MvcConfig.java
@@ -0,0 +1,45 @@
+package chapter11;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class MvcConfig implements WebMvcConfigurer{
+
+	
+	
+	@Override
+	public void configureViewResolvers(ViewResolverRegistry registry) {
+		registry.jsp("/WEB-INF/view/", ".jsp");
+	}
+
+	@Override
+	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+		configurer.enable();
+		
+	}
+
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/main").setViewName("index");
+		// main으로 들어오는건 index이름으로 보여주겠다
+	}
+	
+	@Bean
+	public MessageSource messageSource() {
+		ResourceBundleMessageSource ms = new ResourceBundleMessageSource();
+		ms.setBasename("message.label");
+		ms.setDefaultEncoding("utf-8");
+		
+		return ms;
+	}
+	
+}
